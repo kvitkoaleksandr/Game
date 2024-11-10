b2120ea87a101bd3d53aa1c646a1d7e9dd2957eb
Kvitko Aleksandr <kvitok-993@mail.ru>
2024-11-10 23:33:40 +0300

    1 commit

--- src/main/java/org/example/game/HelloApplication.java
+++ src/main/java/org/example/game/HelloApplication.java
@@ -3,7 +3,9 @@ package org.example.game;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,9 +13,29 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 300);
-        stage.setTitle("Hello App!");
+        Scene scene = new Scene(fxmlLoader.load(), 712, 400);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
+
+        scene.setOnKeyPressed(e -> {
+            if(e.getCode() == KeyCode.SPACE && !HelloController.jump)
+                HelloController.jump = true;
+
+            if(e.getCode() == KeyCode.A)
+                HelloController.left = true;
+
+            if(e.getCode() == KeyCode.D)
+                HelloController.right = true;
+        });
+
+        scene.setOnKeyReleased(e -> {
+            if(e.getCode() == KeyCode.A)
+                HelloController.left = false;
+
+            if(e.getCode() == KeyCode.D)
+                HelloController.right = false;
+        });
+
         stage.show();
     }
 
--- src/main/java/org/example/game/HelloController.java
+++ src/main/java/org/example/game/HelloController.java
@@ -1,11 +1,12 @@
 package org.example.game;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import javafx.animation.*;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import javafx.util.Duration;
+import java.util.ResourceBundle;
 
 public class HelloController {
 
@@ -16,19 +17,53 @@ public class HelloController {
     private URL location;
 
     @FXML
-    private Button mainBtn;
+    private ImageView bj1, bj2, player;
 
-    @FXML
-    private Label mainLabel;
+    private final int BG_WIDTH = 712;
 
-    @FXML
-    void btnClick(ActionEvent event) {
-        mainLabel.setText(mainBtn.getText());
+    private ParallelTransition parallelTransition;
+
+    public static boolean jump = false;
+    public static boolean right = false;
+    public static boolean left = false;
+
+    private int playerSpeed = 3, jumpDownSpeed = 5;
+
+    AnimationTimer timer = new AnimationTimer() {
+        @Override
+        public void handle(long l) {
+            if(jump && player.getLayoutY() > 90f)
+                player.setLayoutY(player.getLayoutY() - playerSpeed);
+                else if(player.getLayoutY() <= 181f) {
+                    jump = false;
+                    player.setLayoutY(player.getLayoutY() + jumpDownSpeed);
+            }
+
+            if (right && player.getLayoutX() < 100f)
+                player.setLayoutX(player.getLayoutX() + playerSpeed);
+            if (left && player.getLayoutX() > 28f)
+                player.setLayoutX(player.getLayoutX() - playerSpeed);
+        }
+    };
+
+        @FXML
+        void initialize() {
+            TranslateTransition bgOneTransition = new TranslateTransition(Duration.millis(5000), (bj1));
+            bgOneTransition.setFromX(0);
+            bgOneTransition.setToX(BG_WIDTH * -1);
+            bgOneTransition.setInterpolator(Interpolator.LINEAR);
+
+
+            TranslateTransition bgTwoTransition = new TranslateTransition(Duration.millis(5000), (bj2));
+            bgTwoTransition.setFromX(0);
+            bgTwoTransition.setToX(BG_WIDTH * -1);
+            bgTwoTransition.setInterpolator(Interpolator.LINEAR);
+
+            parallelTransition = new ParallelTransition(bgOneTransition, bgTwoTransition);
+            parallelTransition.setCycleCount(Animation.INDEFINITE);
+            parallelTransition.play();
+
+            timer.start();
+
+        }
     }
-
-    @FXML
-    void initialize() {
-
-    }
-
-}
